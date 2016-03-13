e1fd19cb20a8b1a310c2093288d595b9127183f8
Sam Cohen <samc1213@gmail.com>
2016-03-12 22:58:08 -0600

    changed config

--- musicproject.py
+++ musicproject.py
@@ -34,4 +34,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
