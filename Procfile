f8f98df38011e4f12db3c29248f9fba3197b07fb
Sam Cohen <samc1213@gmail.com>
2016-03-13 20:10:46 -0500

    oobobo

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python musicproject.py
--- application.py
+++ application.py
@@ -1,45 +1,46 @@
 from flask import Flask, render_template, url_for, request, send_from_directory, redirect
 from werkzeug import secure_filename
-from audiostuff import save_spectrogram
+# from audiostuff import save_spectrogram
 import os
 
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['wav', 'mp3'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('thanks',
                                     filename=filename))
     return render_template('index.html')
 
-@app.route('/thanks/<filename>')
+@application.route('/thanks/<filename>')
 def thanks(filename):
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    filepath = os.path.join(application.config['UPLOAD_FOLDER'], filename)
     context = {'filename': filename, 'filepath': filepath}
     save_spectrogram(filepath, filename)
     return render_template('thanks.html', context = context)
 
-@app.route('/talkover/<filename>')
+@application.route('/talkover/<filename>')
 def talkover(filename):
     context = {}
     return render_template('talkover.html', context = context)
 
-@app.route('/useruploads/<filename>')
+@application.route('/useruploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    application.run(port=port, debug=True)
