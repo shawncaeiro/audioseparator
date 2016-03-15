0890820d328babdf919d4d1f304861e1716d94ba
Sam Cohen <samc1213@gmail.com>
2016-03-14 19:45:45 -0500

    changed port

--- application.py
+++ application.py
@@ -86,5 +86,5 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    # application.run(host='0.0.0.0', port=port, debug=True)
-    application.run(port=port, debug=True)
+    application.run(host='0.0.0.0', port=port, debug=True)
+    # application.run(port=port, debug=True)
