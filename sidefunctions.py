3b62d2d868154f86364f1f869e1e6e2de6c287cc
Sam Cohen <samc1213@gmail.com>
2016-03-14 20:08:22 -0500

    this should screw it up differently...

--- sidefunctions.py
+++ sidefunctions.py
@@ -13,7 +13,7 @@ def combineandsplitsongs(path_to_audio, path_to_voice, path_of_output, path_to_o
     voice, sr = librosa.load(path_to_voice)
     song[0:len(voice)] += voice
     librosa.output.write_wav(path_of_output, song, sr)
-    splitty(path_of_output, path_to_output1, path_to_output2)
+    splitty(path_of_output + 'q', path_to_output1, path_to_output2)
 
 def combinesongs(path_to_audio, path_to_voice, path_of_output):
     song, sr = librosa.load(path_to_audio)
