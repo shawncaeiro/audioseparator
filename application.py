ae3071bc18597ffc4101a4fc2d4fbf05e86fe7c5
Sam Cohen <samc1213@gmail.com>
2016-03-14 23:13:02 -0500

    styling stuff

--- application.py
+++ application.py
@@ -100,6 +100,6 @@ def uploaded_file(filename):
     return send_from_directory(application.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    application.run(host='0.0.0.0', port=port, debug=True)
-    # application.run(port=port, debug=True)
+    port = int(os.environ.get("PORT", 5020))
+    # application.run(host='0.0.0.0', port=port, debug=True)
+    application.run(port=port, debug=True)
